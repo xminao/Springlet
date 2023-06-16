47c06da1e73fdd47e79cd13e298778d1c06fa50f
xminao <xminao@yeah.net>
2023-06-16 15:37:37 +0800

    feat(AroundAop): Around annotation aop.

---
+++ springlet-aop/src/main/java/xyz/xminao/springlet/aop/AroundProxyBeanPostProcessor.java
@@ -0,0 +1,65 @@
+package xyz.xminao.springlet.aop;
+
+import xyz.xminao.springlet.annotation.Around;
+import xyz.xminao.springlet.context.ApplicationContextUtils;
+import xyz.xminao.springlet.context.BeanDefinition;
+import xyz.xminao.springlet.context.BeanPostProcessor;
+import xyz.xminao.springlet.context.ConfigurableApplicationContext;
+import xyz.xminao.springlet.exception.AopConfigException;
+
+import java.lang.reflect.InvocationHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 检测每个Bean实例是否有@Around实例，如果有就根据注解的value查找Bean作为拦截器，创建proxy。
+ * 返回前保存原始Bean的引用，后序ioc注入依赖要注入到原始bean。
+ */
+
+public class AroundProxyBeanPostProcessor implements BeanPostProcessor {
+    // 保存原始Bean，Ioc容器后序需要注入相关依赖和值到原始Bean中
+    Map<String, Object> originBeans = new HashMap<>();
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        Class<?> beanClass = bean.getClass();
+        // 检测@Around注解
+        Around anno = beanClass.getAnnotation(Around.class);
+        if (anno != null) {
+            String handlerName;
+            try {
+                handlerName = (String) anno.annotationType().getMethod("value").invoke(anno); // 通过反射获取value值
+            } catch (ReflectiveOperationException e) {
+                throw new AopConfigException(e);
+            }
+            Object proxy = createProxy(beanClass, bean, handlerName);
+            originBeans.put(beanName, bean);
+            return proxy;
+        } else {
+            return bean;
+        }
+    }
+
+    Object createProxy(Class<?> beanClass, Object bean, String handlerName) {
+        ConfigurableApplicationContext ctx = (ConfigurableApplicationContext) ApplicationContextUtils.getRequiredApplicationContext();
+        BeanDefinition def = ctx.findBeanDefinition(handlerName); // 获取拦截器bean
+        if (def == null) {
+            throw new AopConfigException();
+        }
+        Object handlerBean = def.getInstance();
+        if (handlerBean == null) {
+            handlerBean = ctx.createBeanAsEarlySingleton(def);
+        }
+        if (handlerBean instanceof InvocationHandler handler) {
+            return ProxyResolver.getInstance().createProxy(bean, handler);
+        } else {
+            throw new AopConfigException();
+        }
+    }
+
+    @Override
+    public Object postProcessOnSetProperty(Object bean, String beanName) {
+        Object origin = this.originBeans.get(beanName);
+        return origin != null ? origin : bean;
+    }
+}
---
+++ springlet-aop/src/test/java/xyz/xminao/springlet/around/AroundApplication.java
@@ -0,0 +1,12 @@
+package xyz.xminao.springlet.around;
+
+import xyz.xminao.springlet.annotation.Bean;
+import xyz.xminao.springlet.annotation.ComponentScan;
+import xyz.xminao.springlet.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AroundApplication {
+    @Bean
+    A
+}
