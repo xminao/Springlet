c00dd145df3f950eff0d312d7d175ae63dedf0b0
xminao <xminao@yeah.net>
2023-06-27 19:11:10 +0800

    feat(webmvc): model view demo.

---
+++ springlet-web/src/main/java/xyz/xminao/springlet/web/exception/ServerWebInputException.java
@@ -0,0 +1,20 @@
+package xyz.xminao.springlet.web.exception;
+
+/// 404 错误
+public class ServerWebInputException extends ErrorResponseException {
+    public ServerWebInputException() {
+        super(400);
+    }
+
+    public ServerWebInputException(String message) {
+        super(400, message);
+    }
+
+    public ServerWebInputException(Throwable cause) {
+        super(400, cause);
+    }
+
+    public ServerWebInputException(String message, Throwable cause) {
+        super(400, message, cause);
+    }
+}
